3f0234daac713b25593135370561fd42a94ea8b0
dedonopradonodedelivery-cloud <dedonopradonodedelivery@gmail.com>
2025-12-05 14:19:43 -0300

    Add files via upload

--- src/components/SpinWheelView_fixed.tsx
+++ src/components/SpinWheelView_fixed.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const SpinWheelView_fixed: React.FC = () => {
-  return null;
-};
+const SpinWheelView_fixed: React.FC = () => null;
 
 export default SpinWheelView_fixed;
