32db9406a82d10856501eacb6d4f7571ddbf20b9
dedonopradonodedelivery-cloud <dedonopradonodedelivery@gmail.com>
2025-12-06 18:58:17 -0300

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Se você tiver um CSS global do Vite/Tailwind, importa aqui:
+// import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
